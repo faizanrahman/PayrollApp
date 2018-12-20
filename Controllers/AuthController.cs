3e00a5963cb52ee318585577002d2b32e2df381b
Meda <meda.serenaite@gmail.com>
2018-12-20 10:39:31 -0600

    Working on LogIn

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,62 +5,77 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using PayrollApp.ViewModels.LoginViewModel;
+using PayrollApp.Data;
+using PayrollApp.Dtos;
+using PayrollApp.Models;
 
-namespace PayrollApp.Controllers
-{
-    [Route("api/[controller]")]
+namespace PayrollApp.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
-    {
-        private object _context;
-
-        // // GET api/values
-        // [HttpGet]
-        // public ActionResult<IEnumerable<string>> Get()
-        // {
-        //     return new string[] { "value1", "value2" };
-        // }
-
-
+    public class AuthController : ControllerBase {
+        // private object _context;
+        private DataContext _context;
+        public AuthController (DataContext context) {
+            _context = context;
+        }
         // ---------------------- L O G I N -------------------
+        [HttpPost ("login")]
+        public IActionResult Login (UserForLoginDto form) {
+            System.Console.WriteLine (form.Email);
+            System.Console.WriteLine (form.Password);
+            System.Console.WriteLine ("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 
-        [HttpPost]
-        public IActionResult Login(LoginViewModel LoginUser) 
-        {
-            if(!ModelState.IsValid)
-            {
-               return View("Index");
-            }
-            User userinDB = _context.Users.FirstOrDefault(u=>u.Email == LoginUser.LoginEmail);
-            
-            if(userinDB is null)
-            {
-                ModelState.AddModelError("Email","Invalid Email");
-                return View("Index");
+            var response = new {
+                name = "fsfasfaf",
+                email = "fdjaslkj@cd.com"
+            };
+            int status = 1;
+
+            var user = _context.Users.FirstOrDefault (u => u.email == form.Email);
+            if (user == null) {
+                System.Console.WriteLine ("Login failed");
+                return BadRequest (status);
+            } else {
+                System.Console.WriteLine (user.UserId);
+                System.Console.WriteLine (user.firstName);
+                // User user = _context.Users.FirstOrDefault(u=>u.Email == form.email);
+                
+                return Ok (user);
             }
 
-            PasswordHasher<LoginViewModel> hasher = new PasswordHasher<LoginViewModel> ();
-            var result = hasher.VerifyHashedPassword (LoginUser, userinDB.Password, LoginUser.LoginPassword);
-            if (result == 0)
-            {
-                ModelState.AddModelError ("LoginEmail", "Invalid Login");
-                return View ("Index");
-            }
-            User loggedIn = _context.Users.FirstOrDefault (u => u.Email == LoginUser.LoginEmail);
-            
-            HttpContext.Session.SetInt32 ("ID", loggedIn.UserID);
-            HttpContext.Session.SetString("UserName", loggedIn.FirstName);
-            HttpContext.Session.SetString("Login", "true");
-            return RedirectToAction("Home");
+            // if(!ModelState.IsValid)
+            // {
+            //    return "Value is " + id;
+            // }
+            // User userinDB = _context.Users.FirstOrDefault(u=>u.Email == LoginUser.LoginEmail);
+
+            // if(userinDB is null)
+            // {
+            //     ModelState.AddModelError("Email","Invalid Email");
+            //     return View("Index");
+            // }
+
+            // PasswordHasher<LoginViewModel> hasher = new PasswordHasher<LoginViewModel> ();
+            // var result = hasher.VerifyHashedPassword (LoginUser, userinDB.Password, LoginUser.LoginPassword);
+            // if (result == 0)
+            // {
+            //     ModelState.AddModelError ("LoginEmail", "Invalid Login");
+            //     return View ("Index");
+            // }
+            // User loggedIn = _context.Users.FirstOrDefault (u => u.Email == LoginUser.LoginEmail);
+
+            // HttpContext.Session.SetInt32 ("ID", loggedIn.UserID);
+            // HttpContext.Session.SetString("UserName", loggedIn.FirstName);
+            // HttpContext.Session.SetString("Login", "true");
+            // return RedirectToAction("Home");
         }
-         // ---------------------- L O G O U T -------------------
-        [HttpGet("{logout}")]
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Clear();
-            return RedirectToAction("Index");
-        }
+
     }
 }
-
+//  ---------------------- L O G O U T -------------------
+// [HttpGet("{logout}")]
+// public IActionResult Logout()
+// {
+//     HttpContext.Session.Clear();
+//     return RedirectToAction("Index");
+// }
---
+++ Migrations/20181219193409_MigratingHere.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PayrollApp.Migrations
+{
+    public partial class MigratingHere : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
